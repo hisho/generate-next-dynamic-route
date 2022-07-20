f380a60d106a63ca37843aae6e09245d9d1f649b
hisho <hisho.web@gmail.com>
2022-07-20 10:26:06 +0900

    ✨ generateした結果を追加

---
+++ debug/next/src/lib/$dynamicPaths.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+// prettier-ignore
+export const dynamicPaths: {
+article_id: string[],
+categories_id: string[],
+post_id: string,
+comment_id: string
+} = {
+	"article_id": [],
+	"categories_id": [],
+	"post_id": "",
+	"comment_id": ""
+}
+// prettier-ignore
+export type DynamicPaths = typeof dynamicPaths
