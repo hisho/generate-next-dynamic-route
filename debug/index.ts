c1ad8ff318727226fb90c7ed0c785ad779f36124
hisho <hisho.web@gmail.com>
2022-07-20 10:25:37 +0900

    🔥 不要な余白削除

--- debug/index.ts
+++ debug/index.ts
@@ -1,5 +1,5 @@
 import { exec } from 'child_process'
 
-exec(`cd debug/next &&  node ./../../bin/index.js`, (_, stdout, stderr) =>
+exec(`cd debug/next && node ./../../bin/index.js`, (_, stdout, stderr) =>
   console.log(stdout, stderr)
 )
